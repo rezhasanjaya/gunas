f263110ad1391e8359af785942436ef6b844cb25
Nasthasa Wulan <nasthasa.10119276@mahasiswa.unikom.ac.id>
2023-02-21 01:36:23 +0700

    updates

---
+++ application/views/gamifikasi/assignment.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <h1>Your Assignment</h1>
+	<p>Selesaikan pekerjaanmu dalam melayani pengunjung.</p>
+    <div class="card">
+        <div class="card-header">
+            Pelayanan Pengunjung 1
+        </div>
+        <div class="card-body">
+            <p>Pilih Nama Pengunjung Yang Sudah Dilayani</p>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Pilih Pengunjung
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ application/views/gamifikasi/mission.php
@@ -0,0 +1,57 @@
+    <div class="container">
+        <h1>Achievement Page</h1>
+	    <p>Here are your mission</p>
+        <a href="<?=base_url() ?>PegawaiPage/assignment">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="assets/img/deluxe.jpg" class="img-fluid rounded-start" alt="gambar1">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Pelayanan Kepada Pengunjung</h5>
+                        <p class="card-text">Selesaikan tugasmu dalam melayani pengunjung hotel.</p>
+                        <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-info" style="width: 50%"></div>
+                        </div>
+                        <p class="card-text"><small class="text-muted">Finished : 5/10</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </a>
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-info" style="width: 50%"></div>
+                        </div>
+                        <p class="card-text"><small class="text-muted">Finished : 5/10</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar bg-info" style="width: 50%"></div>
+                        </div>
+                        <p class="card-text"><small class="text-muted">Finished : 5/10</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
